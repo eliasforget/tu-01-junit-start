68fa8ad2e8e5b9039fc85c36536a33149f27db1c
eliasforget <elias.forget@supdevinci-edu.fr>
2026-02-04 15:16:54 +0100

    TP J1.3 - Source de données CSV

--- src/test/java/org/sebsy/levenshtein/DistanceLevenshteinTest.java
+++ src/test/java/org/sebsy/levenshtein/DistanceLevenshteinTest.java
@@ -3,6 +3,9 @@ package org.sebsy.levenshtein;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,4 +43,13 @@ class DistanceLevenshteinTest {
     void distance_java_jee() {
         assertEquals((int) cas[2][2], distance.calculerDistance((String) cas[2][0], (String) cas[2][1]));
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/org/sebsy/levenshtein/DistanceLevenshtein.csv",
+            delimiter = ';',
+            numLinesToSkip = 1)
+    void calculerDistance_depuisCsv(String mot1, String mot2, int distanceAttendue) {
+        assertEquals(distanceAttendue, distance.calculerDistance(mot1, mot2));
+    }
+
 }
